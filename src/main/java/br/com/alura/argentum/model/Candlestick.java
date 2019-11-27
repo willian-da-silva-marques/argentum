eefb117460226528d78a6ddc755b9232e71805b3
willian.marques <willian.marques@CTNDELL23W10Y1.pampa.compasso>
2019-11-27 09:16:37 -0300

    implementando o modelo da classe Candlestick

---
+++ src/main/java/br/com/alura/argentum/model/Candlestick.java
@@ -0,0 +1,57 @@
+package br.com.alura.argentum.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public final class Candlestick implements Serializable {
+
+	private static final long serialVersionUID = -256521810064610274L;
+
+	private final double abertura;
+	private final double fechamento;
+	private final double maximo;
+	private final double minimo;
+	private final double volume;
+	private final LocalDateTime data;
+	
+	public Candlestick(double abertura, double fechamento, double maximo, double minimo, double volume,	LocalDateTime data) {
+		this.abertura = abertura;
+		this.fechamento = fechamento;
+		this.maximo = maximo;
+		this.minimo = minimo;
+		this.volume = volume;
+		this.data = data;
+	}
+
+	public double getAbertura() {
+		return abertura;
+	}
+
+	public double getFechamento() {
+		return fechamento;
+	}
+
+	public double getMaximo() {
+		return maximo;
+	}
+
+	public double getMinimo() {
+		return minimo;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	public boolean isAlta() {
+		return this.fechamento > this.abertura;
+	}
+	
+	public boolean isBaixa() {
+		return this.fechamento < this.abertura;
+	}
+}
